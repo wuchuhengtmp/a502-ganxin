898e662021f00d79cd2e39b2afabbb3abba9c2e6
wuchuheng <wuchuheng@163.com>
2021-05-24 16:03:59 +0800

    feat:初始公司相关表

---
+++ http-api/app/models/companies/config.go
@@ -0,0 +1,29 @@
+/**
+ * @Desc    公司表
+ * @Author  wuchuheng<wuchuheng@163.com>
+ * @Blog    https://wuchuheng.com
+ * @wechat  wc20030318
+ * @DATE    2021/5/24
+ * @Listen  MIT
+ */
+package companies
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Companies struct {
+	ID               int64     `json:"id"`
+	Name             string    `json:"name"`
+	PinYin           string    `json:"pinyin" gorm:"comment:用于型钢编码生成"`
+	Symbol           string    `json:"symbol" gorm:"comment:APP 企业宗旨"`
+	LogoFileId       int64     `json:"logoFileId" gorm:"comment:文件id"`
+	BackgroundFileId int64     `json:"backgroundFileId" gorm:"comment:app背景文件id"`
+	State            bool      `json:"state" gorm:"comment:账号状态"`
+	Phone 			 string  	`json:"phone" gorm:"comment:公司的电话"`
+	Wechat 			 string  	`json:"wechat" gorm:"comment:公司的微信"`
+	StartedAt        time.Time `json:"started_at" gorm:"comment:开始时间"`
+	EndedAt          time.Time `json:"ended_at" gorm:"comment:结束时间"`
+	gorm.Model
+}
---
+++ http-api/app/models/file/config.go
@@ -0,0 +1,15 @@
+/**
+ * @Desc    文件模型
+ * @Author  wuchuheng<wuchuheng@163.com>
+ * @Blog    https://wuchuheng.com
+ * @wechat  wc20030318
+ * @DATE    2021/5/24
+ * @Listen  MIT
+ */
+package file
+
+type Files struct {
+	ID int64 `json:"id"`
+	Path string `json:"path" gorm:"comment:文件路径"`
+	Disk string `json:"disk" gorm:"comment:硬盘,default:local"`
+}
