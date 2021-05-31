7d300ddb1949c5d68b8444e58f7de2de9f461649
wuchuheng <wuchuheng@163.com>
2021-05-31 09:30:18 +0800

    fix:规划文件名

---
+++ http-api/app/http/graph/schema/mutation_resolver/edit_company.go
@@ -0,0 +1,9 @@
+/**
+ * @Desc    The mutation_resolver is part of http-api
+ * @Author  wuchuheng<wuchuheng@163.com>
+ * @Blog    https://wuchuheng.com
+ * @wechat  wc20030318
+ * @DATE    2021/5/31
+ * @Listen  MIT
+ */
+package mutation_resolver
