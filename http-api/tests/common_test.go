4a6f14dec2b3e08f26026a2f06cfbb1a26701abe
wuchuheng <wuchuheng@163.com>
2021-06-04 11:10:18 +0800

    test:为"获取角色"添加集成测试

---
+++ http-api/tests/common_test.go
@@ -0,0 +1,28 @@
+/**
+ * @Desc    无角色鉴权限制的公共接口集成测试
+ * @Author  wuchuheng<wuchuheng@163.com>
+ * @Blog    https://wuchuheng.com
+ * @wechat  wc20030318
+ * @DATE    2021/6/4
+ * @Listen  MIT
+ */
+package tests
+
+import (
+	"testing"
+)
+
+func TestGetRoleList(t *testing.T) {
+	q := `
+		query getRoleListQuery {
+		  getRoleList{
+			id
+			name
+			tag
+		  }
+		}
+	`
+	v := map[string]interface{}{}
+	_, err := graphReqClient(q, v, "")
+	hasError(t, err)
+}
