00f535433db74a987b10c49746cf574cf1f32bcd
wuchuheng <wuchuheng@163.com>
2021-06-22 17:13:10 +0800

    fix:修复"快速查询接口中"型钢数据少了编码字段

--- http-api/app/http/graph/generated/generated.go
+++ http-api/app/http/graph/generated/generated.go
@@ -341,6 +341,7 @@ type ComplexityRoot struct {
 	}
 
 	SteelItem struct {
+		Code                 func(childComplexity int) int
 		CompanyId            func(childComplexity int) int
 		CreateUser           func(childComplexity int) int
 		CreatedAt            func(childComplexity int) int
@@ -2017,6 +2018,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 
 		return e.complexity.SteelInProject.UseDays(childComplexity), true
 
+	case "SteelItem.code":
+		if e.complexity.SteelItem.Code == nil {
+			break
+		}
+
+		return e.complexity.SteelItem.Code(childComplexity), true
+
 	case "SteelItem.companyId":
 		if e.complexity.SteelItem.CompanyId == nil {
 			break
@@ -3065,6 +3073,8 @@ type SteelItem {
     steelInProject: [SteelInProject]!
     """ 维修经历 """
     steelInMaintenance: [MaintenanceRecordItem]!
+    """ 型钢编码 """
+    code: String!
     """ 首次入库时间 """
     createdAt: Time!
 }
@@ -12215,6 +12225,41 @@ func (ec *executionContext) _SteelItem_steelInMaintenance(ctx context.Context, f
 	return ec.marshalNMaintenanceRecordItem2ᚕᚖhttpᚑapiᚋappᚋmodelsᚋmaintenance_recordᚐMaintenanceRecord(ctx, field.Selections, res)
 }
 
+func (ec *executionContext) _SteelItem_code(ctx context.Context, field graphql.CollectedField, obj *steels.Steels) (ret graphql.Marshaler) {
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	fc := &graphql.FieldContext{
+		Object:     "SteelItem",
+		Field:      field,
+		Args:       nil,
+		IsMethod:   false,
+		IsResolver: false,
+	}
+
+	ctx = graphql.WithFieldContext(ctx, fc)
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.Code, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(string)
+	fc.Result = res
+	return ec.marshalNString2string(ctx, field.Selections, res)
+}
+
 func (ec *executionContext) _SteelItem_createdAt(ctx context.Context, field graphql.CollectedField, obj *steels.Steels) (ret graphql.Marshaler) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -17030,6 +17075,11 @@ func (ec *executionContext) _SteelItem(ctx context.Context, sel ast.SelectionSet
 				}
 				return res
 			})
+		case "code":
+			out.Values[i] = ec._SteelItem_code(ctx, field, obj)
+			if out.Values[i] == graphql.Null {
+				atomic.AddUint32(&invalids, 1)
+			}
 		case "createdAt":
 			out.Values[i] = ec._SteelItem_createdAt(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
