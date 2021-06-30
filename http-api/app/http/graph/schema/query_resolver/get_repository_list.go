a7254bea4c864fb83145c7e1e35e50b094cb6c14
wuchuheng <wuchuheng@163.com>
2021-06-30 18:32:32 +0800

    feat:为获取仓库列表添加仓库管理员设备端只能获取息仓库列表的限制

--- http-api/app/http/graph/schema/query_resolver/get_repository_list.go
+++ http-api/app/http/graph/schema/query_resolver/get_repository_list.go
@@ -10,21 +10,43 @@ package query_resolver
 
 import (
 	"context"
+	"fmt"
 	"http-api/app/http/graph/auth"
 	"http-api/app/http/graph/errors"
 	"http-api/app/models/repositories"
+	"http-api/app/models/repository_leader"
+	"http-api/app/models/roles"
 	"http-api/app/models/users"
+	"http-api/pkg/model"
 )
 func (*QueryResolver)GetRepositoryList(ctx context.Context) ([]*repositories.Repositories, error) {
 	var res []*repositories.Repositories
 	repositoryModel := repositories.Repositories{}
 	me := auth.GetUser(ctx)
-	repositoryList, err := repositoryModel.GetAllRepositoryByCompanyId(me.CompanyId)
-	if err != nil {
-		return res, errors.ServerErr(ctx, err)
+	// 设备上的仓库管理员
+	if auth.IsDevice(ctx) && me.RoleId == roles.RoleRepositoryAdminId  {
+		repositoryLeaderTable := repository_leader.RepositoryLeader{}.TableName()
+		repositoryTable := repositories.Repositories{}.TableName()
+		var res []*repositories.Repositories
+		err := model.DB.Model(&repositories.Repositories{}).
+			Select(fmt.Sprintf("%s.*", repositoryTable)).
+			Joins(fmt.Sprintf("join %s ON %s.repository_id = %s.id", repositoryLeaderTable, repositoryLeaderTable, repositoryTable)).
+			Where(fmt.Sprintf("%s.uid = ?", repositoryLeaderTable), me.Id).
+			Find(&res).
+			Error
+		if err != nil {
+			return res, errors.ServerErr(ctx, err)
+		}
+		return res, nil
+	} else {
+		repositoryList, err := repositoryModel.GetAllRepositoryByCompanyId(me.CompanyId)
+		if err != nil {
+			return res, errors.ServerErr(ctx, err)
+		}
+
+		return repositoryList, nil
 	}
 
-	return repositoryList, nil
 }
 
 type RepositoryItemResolver struct {}
