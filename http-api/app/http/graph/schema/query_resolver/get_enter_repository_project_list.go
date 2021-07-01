816d16fd3e9383039e6b65deb3dec6cd67c3067d
wuchuheng <wuchuheng@163.com>
2021-07-01 17:01:30 +0800

    fix:修复"获取可归库的项目列表" 数据重复的问题

--- http-api/app/http/graph/schema/query_resolver/get_enter_repository_project_list.go
+++ http-api/app/http/graph/schema/query_resolver/get_enter_repository_project_list.go
@@ -12,6 +12,7 @@ import (
 	"context"
 	"fmt"
 	"http-api/app/http/graph/auth"
+	"http-api/app/http/graph/errors"
 	"http-api/app/models/order_specification"
 	"http-api/app/models/order_specification_steel"
 	"http-api/app/models/orders"
@@ -30,16 +31,36 @@ func (*QueryResolver) GetEnterRepositoryProjectList(ctx context.Context) (res []
 	repositoryLeaderTable := repository_leader.RepositoryLeader{}.TableName()
 	orderSpecificationTable := order_specification.OrderSpecification{}.TableName()
 	orderSpecificationSteelTable := order_specification_steel.OrderSpecificationSteel{}.TableName()
-	model.DB.Model(&projects.Projects{}).
-		Select(fmt.Sprintf("%s.*", projectTable)).
-		Joins(fmt.Sprintf("join %s ON %s.project_id = %s.id", orderTable, orderTable, projectTable)).
+	var orderItems []*orders.Order
+	err = model.DB.Model(&orders.Order{}).
+		Select(fmt.Sprintf("%s.*", orders.Order{}.TableName())).
 		Joins(fmt.Sprintf("join %s ON %s.id = %s.repository_id", repositoryTable, repositoryTable, orderTable)).
 		Joins(fmt.Sprintf("join %s ON %s.repository_id = %s.id", repositoryLeaderTable, repositoryLeaderTable, repositoryTable)).
 		Joins(fmt.Sprintf("join %s ON %s.order_id = %s.id", orderSpecificationTable, orderSpecificationTable, orderTable)).
 		Joins(fmt.Sprintf("join %s ON %s.order_specification_id = %s.id", orderSpecificationSteelTable, orderSpecificationSteelTable, orderSpecificationTable)).
 		Where(fmt.Sprintf("%s.uid = ?", repositoryLeaderTable), me.Id).
 		Where(fmt.Sprintf("%s.state = ?", orderSpecificationSteelTable), steels.StateProjectOnTheStoreWay).
-		Find(&res)
+		Where(fmt.Sprintf("%s.company_id = ?", orderTable), me.CompanyId).
+		Find(&orderItems).
+		Error
+	if err != nil {
+		return res, errors.ServerErr(ctx, err)
+	}
+	orderIdMapId := make(map[int64]int64)
+	var orderIds  []int64
+	for _, o := range orderItems {
+		if _, ok := orderIdMapId[o.Id]; !ok {
+			orderIds = append(orderIds, o.Id)
+			orderIdMapId[o.Id] = o.Id
+		}
+	}
+	err = model.DB.Model(&projects.Projects{}).
+		Joins(fmt.Sprintf("join %s ON %s.project_id = %s.id", orderTable, orderTable, projectTable)).
+		Where(fmt.Sprintf("%s.id in ?", orderTable ), orderIds).
+		Find(&res).Error
+	if err != nil {
+		return res, errors.ServerErr(ctx, err)
+	}
 
 	return
 }
