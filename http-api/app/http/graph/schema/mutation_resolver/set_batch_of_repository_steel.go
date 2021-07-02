2c30ef54e3f1783de7d0d80243a4d862ad53f21f
wuchuheng <wuchuheng@163.com>
2021-07-02 18:37:01 +0800

    fix：修复仓库型钢批量修改 没有写入日志的问题

--- http-api/app/http/graph/schema/mutation_resolver/set_batch_of_repository_steel.go
+++ http-api/app/http/graph/schema/mutation_resolver/set_batch_of_repository_steel.go
@@ -10,9 +10,12 @@ package mutation_resolver
 
 import (
 	"context"
+	"gorm.io/gorm"
+	"http-api/app/http/graph/auth"
 	"http-api/app/http/graph/errors"
 	graphModel "http-api/app/http/graph/model"
 	"http-api/app/http/graph/schema/requests"
+	"http-api/app/models/steel_logs"
 	"http-api/app/models/steels"
 	"http-api/pkg/model"
 )
@@ -21,16 +24,43 @@ func (*MutationResolver) SetBatchOfRepositorySteel(ctx context.Context, input gr
 	if err := requests.ValidateSetBatchOfRepositorySteelRequest(ctx, input); err != nil {
 		return res, errors.ValidateErr(ctx, err)
 	}
-	err = model.DB.Model(&steels.Steels{}).
-		Where("identifier IN ?", input.IdentifierList).
-		Update("manufacturer_id", input.ManufacturerID).
-		Update("material_manufacturer_id", input.MaterialManufacturersID).
-		Update("produced_date", input.ProducedAt).
-		Update("specification_id", input.SpecificationID).
-		Error
+	me := auth.GetUser(ctx)
+	err = model.DB.Transaction(func(tx *gorm.DB) error {
+		for _, identifier := range input.IdentifierList {
+			// 修改
+			err = model.DB.Model(&steels.Steels{}).
+				Where("identifier = ?", identifier).
+				Update("manufacturer_id", input.ManufacturerID).
+				Update("material_manufacturer_id", input.MaterialManufacturersID).
+				Update("produced_date", input.ProducedAt).
+				Update("specification_id", input.SpecificationID).
+				Error
+			if err != nil {
+				return err
+			}
+			// 型钢日志
+			s := steels.Steels{}
+			err = tx.Model(&s).Where("identifier = ?", identifier).
+				Where("company_id = ?", me.CompanyId).
+				First(&s).Error
+			if err != nil {
+				return err
+			}
+			err = tx.Create(&steel_logs.SteelLog{
+				Type:    steel_logs.EditType,
+				SteelId: s.ID,
+				Uid:     me.Id,
+			}).Error
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 	if err != nil {
 		return res, errors.ServerErr(ctx, err)
 	}
+
 	err = model.DB.Model(&steels.Steels{}).
 		Where("identifier in ?", input.IdentifierList).
 		Find(&res).Error
--- http-api/app/models/steel_logs/config.go
+++ http-api/app/models/steel_logs/config.go
@@ -20,19 +20,20 @@ type SteelLog struct {
 type SteelLogType string
 
 const (
-	CreateType SteelLogType = "create" // 创建
-	OutSteelType SteelLogType = "outSteel" //  出库了
-	EnterWorkshopType SteelLogType = "enterWorkshop"  // 入场了
-	InstallationType SteelLogType = "InstallationType" // 安装
-	ChangeType SteelLogType = "changeType" // 修改型钢状态
-	OutOfWorkshop SteelLogType = "SteelLogType" //  出场
-	EnterRepositoryType SteelLogType  = "EnterRepositoryType" // 入场状态
-	CrapSteelType SteelLogType = "CrapSteelType" // 型钢报废
+	CreateType          SteelLogType = "create"              // 创建
+	OutSteelType        SteelLogType = "outSteel"            //  出库了
+	EnterWorkshopType   SteelLogType = "enterWorkshop"       // 入场了
+	InstallationType    SteelLogType = "InstallationType"    // 安装
+	ChangeType          SteelLogType = "changeType"          // 修改型钢状态
+	OutOfWorkshop       SteelLogType = "SteelLogType"        //  出场
+	EnterRepositoryType SteelLogType = "EnterRepositoryType" // 入场状态
+	CrapSteelType       SteelLogType = "CrapSteelType"       // 型钢报废
+	EditType            SteelLogType = "editType"            // 修改
 )
 
 // 操作类型映射说明
-var TypeMapName = map[SteelLogType]string {
-	CreateType: "型钢入库",
+var TypeMapName = map[SteelLogType]string{
+	CreateType:   "型钢入库",
 	OutSteelType: "型钢出库",
 }
 
