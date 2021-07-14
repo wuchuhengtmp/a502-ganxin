19c3d3d5c618503806abbf22b3cc953aabfbb877
wuchuheng <wuchuheng@163.com>
2021-07-14 10:32:40 +0800

    fix:修复"获取验证码"没有限流的问题

--- http-api/app/http/graph/schema/mutation_resolver/create_code.go
+++ http-api/app/http/graph/schema/mutation_resolver/create_code.go
@@ -6,8 +6,10 @@ import (
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
 	"http-api/app/http/graph/errors"
 	graphModel "http-api/app/http/graph/model"
-	"http-api/app/http/graph/schema/requests"
 	"http-api/app/models/configs"
+	"http-api/app/models/users"
+	"http-api/pkg/model"
+	"regexp"
 	"time"
 )
 
@@ -16,11 +18,48 @@ type SmsKeyMapCodeType map[string]struct {
 	CreatedAt time.Time
 	Phone     string
 }
-
+/**
+ * 短信验证码缓存位置
+ */
 var SmsKeyMapCode = make(SmsKeyMapCodeType)
 
+/**
+ * 手机号最近获取到验证码的时间
+ */
+var PhoneMapLastTimeForSMS = make(map[string]time.Time)
+
+func init()  {
+	// 清理过期的验证码缓存数据
+	go func() {
+		ticker := time.NewTicker(time.Minute * 30)
+		for {
+			<- ticker.C
+			for key, item := range SmsKeyMapCode {
+				timeLen := time.Now().Unix() - item.CreatedAt.Unix()
+				if timeLen > 60 * 30 {
+					delete(SmsKeyMapCode, key)
+				}
+			}
+		}
+	}()
+	// 清理没用的上次获取手机号的时间缓存数据
+	go func() {
+		secondLen := time.Duration( 60)
+		ticker := time.NewTicker(time.Second * secondLen)
+		for {
+			<- ticker.C
+			for key, item := range PhoneMapLastTimeForSMS {
+				if item.Unix() + int64(secondLen) < time.Now().Unix() {
+					delete(PhoneMapLastTimeForSMS, key)
+				}
+			}
+		}
+	}()
+
+}
+
 func (*MutationResolver) CreateCode(ctx context.Context, input graphModel.GetCodeForForgetPasswordInput) (*graphModel.GetCodeForForgetPasswordRes, error) {
-	if err := requests.ValidateCreateCodeRequest(ctx, input); err != nil {
+	if err := ValidateCreateCodeRequest(input); err != nil {
 		return nil, errors.ValidateErr(ctx, err)
 	}
 	accessKey := configs.GetGlobalVal(configs.SMS_ACCESS_KEY)
@@ -44,6 +83,7 @@ func (*MutationResolver) CreateCode(ctx context.Context, input graphModel.GetCod
 		CreatedAt time.Time
 		Phone     string
 	}{Code: code, CreatedAt: time.Now(), Phone: input.Phone}
+	PhoneMapLastTimeForSMS[input.Phone] = time.Now()
 
 	_, err = client.SendSms(request)
 	if err != nil {
@@ -54,4 +94,25 @@ func (*MutationResolver) CreateCode(ctx context.Context, input graphModel.GetCod
 	}
 
 	return &res, nil
+}
+
+
+func ValidateCreateCodeRequest(input graphModel.GetCodeForForgetPasswordInput) error {
+	_, err := regexp.Match("^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\\d{8}$", []byte(input.Phone) )
+	if err != nil {
+		return fmt.Errorf("手机号为: %s 不是正确的手机号", input.Phone)
+	}
+	userItem := users.Users{}
+	err = model.DB.Model(&userItem).Where("phone = ?", &input.Phone).Find(&userItem).Error
+	if err != nil {
+		return fmt.Errorf("没有手机号为: %s 的用户", input.Phone)
+	}
+	lastTime, ok := PhoneMapLastTimeForSMS[input.Phone]
+	if ok {
+		if time.Now().Unix() - lastTime.Unix() < 60{
+			return fmt.Errorf("请在1分钟后，重新获取")
+		}
+	}
+
+	return nil
 }
--- http-api/app/http/graph/schema/requests/create_code_request.go
+++ http-api/app/http/graph/schema/requests/create_code_request.go
@@ -8,25 +8,3 @@
  */
 package requests
 
-import (
-	"context"
-	"fmt"
-	graphModel "http-api/app/http/graph/model"
-	"http-api/app/models/users"
-	"http-api/pkg/model"
-	"regexp"
-)
-
-func ValidateCreateCodeRequest(ctx context.Context, input graphModel.GetCodeForForgetPasswordInput) error {
-	_, err := regexp.Match("^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\\d{8}$", []byte(input.Phone) )
-	if err != nil {
-		return fmt.Errorf("手机号为: %s 不是正确的手机号", input.Phone)
-	}
-	userItem := users.Users{}
-	err = model.DB.Model(&userItem).Where("phone = ?", &input.Phone).Find(&userItem).Error
-	if err != nil {
-		return fmt.Errorf("没有手机号为: %s 的用户", input.Phone)
-	}
-
-	return nil
-}
