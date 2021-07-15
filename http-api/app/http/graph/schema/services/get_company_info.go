cdb5bf30af93d2e93c4a8c3df629276580244fbf
wuchuheng <wuchuheng@163.com>
2021-07-15 09:58:18 +0800

    fix:修复"获取公司信息"接口的电话和微信

--- http-api/app/http/graph/schema/services/get_company_info.go
+++ http-api/app/http/graph/schema/services/get_company_info.go
@@ -10,13 +10,15 @@ package services
 
 import (
 	"context"
+	"http-api/app/http/graph/auth"
 	graphModel "http-api/app/http/graph/model"
+	"http-api/app/models/companies"
 	"http-api/app/models/configs"
 	"http-api/app/models/files"
 	"http-api/pkg/model"
 )
 
-func GetCompanyInfo (ctx context.Context) (*graphModel.GetCompnayInfoRes, error) {
+func GetCompanyInfo(ctx context.Context) (*graphModel.GetCompnayInfoRes, error) {
 	fid := configs.GetVal(configs.TUTOR_FILE_NAME, ctx)
 	fileItem := files.File{}
 	if err := model.DB.Model(&fileItem).Where("id = ?", fid).First(&fileItem).Error; err != nil {
@@ -24,10 +26,16 @@ func GetCompanyInfo (ctx context.Context) (*graphModel.GetCompnayInfoRes, error)
 	}
 	res := graphModel.GetCompnayInfoRes{}
 	res.Tutor = &graphModel.FileItem{
-		ID: fileItem.ID,
+		ID:  fileItem.ID,
 		URL: fileItem.GetUrl(),
 	}
-	res.Phone = configs.GetVal(configs.PHONE_NAME, ctx)
-	res.Wechat = configs.GetVal(configs.WECHAT_NAME, ctx)
+	companyItem := companies.Companies{}
+	me := auth.GetUser(ctx)
+	if err := model.DB.Model(&companyItem).Where("id = ?", me.CompanyId).First(&companyItem).Error; err != nil {
+		return nil, err
+	}
+
+	res.Phone = companyItem.Phone
+	res.Wechat = companyItem.Wechat
 	return &res, nil
 }
